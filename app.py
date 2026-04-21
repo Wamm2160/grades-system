bcddb6f120f5621a07d509fae8665227ba0895f3
Wamm2160 <wamm2160@gmail.com>
2026-04-21 02:26:55 -0600

    feat: add CLI system with menu and user intersection

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 def add_grade(grades, grade):
     if grade < 0 or grade > 10: 
         raise ValueError("Grade must be between 0 and 10")
@@ -9,9 +10,61 @@ def calculate_average(grades):
         return 0
     return sum(grades) / len(grades)
 
+def get_max_grade(grades):
+    if len(grades) == 0:
+        return 0
+    return max(grades)
+
+def numbers_of_grades(grades):
+    return len(grades)
+
+def clear_all_grades():
+    return []
+
+def menu():
+    print("\n--- Grade System ---")
+    print("1. Add Grade")
+    print("2. Show Average")
+    print("3. Show max grade")
+    print("4. Show how many grades have been added")
+    print("5. Clear all grades")
+    print("6. Exit")
+
+
+
 grades = []
 
-grades = add_grade(grades, 8)
-grades = add_grade(grades, 10)
+while True:
+    menu()
+    option = input("choose an option: ")
+    
+    if option == "1":
+        try:
+            grade = float(input("Enter grade (0-10): "))
+            grades = add_grade(grades, grade) 
+            print("Grade added successfully.")
+        except ValueError:
+            print("Invalid input. Try again.")
+            
+    elif option == "2":
+        print("Average:", calculate_average(grades))
+    
+    elif option == "3":
+        print ("Max grade:", get_max_grade(grades))
+    
+    elif option == "4":
+        if len(grades) == 0:
+            print("No grade added yet.")
+        else:
+            print("Number of grades added:", numbers_of_grades(grades))
+    
+    elif option == "5":
+        grades = clear_all_grades()
+        print("All grades cleared.")
+        
+    elif option == "6":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid option")
 
-print("Average:", calculate_average(grades))
